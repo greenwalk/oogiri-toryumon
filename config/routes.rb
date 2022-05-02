b157ecb44f7a76d940a33c26e796721ec6fb6f02
greenwalk <93359072+greenwalk@users.noreply.github.com>
2022-05-02 18:03:48 +0900

    テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'results#index'
-  resources :results, only: [:index]
+  resources :results, only: [:index] do
+    collection do
+      get 'index1'
+      get 'page1_1'
+      get 'page1_2'
+    end
+  end
 end
