307ffc6bb771847510b58d0afd077ae09c56d4da
greenwalk <tomitaka.tatsumi2021@gmail.com>
2022-05-10 03:47:18 +0900

    第二回大喜利登竜門

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   root to: 'results#index'
   resources :results, only: [:index] do
     collection do
+      # 第一回
       get 'index1'
       get 'page1_1'
       get 'page1_2'
+      # 第二回
+      get 'index2'
+      get 'page2_1'
+      get 'page2_2'
     end
   end
 end
